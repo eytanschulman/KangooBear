9985c6e5647f761e11251a2d2bd2de55311f1ba1
Eytan Schulman <eytanschulman@gmail.com>
2016-01-23 00:14:41 -0500

    Initial Commit

---
+++ KangooBear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KangooBear
+//
+//  Created by Eytan Schulman on 1/23/16.
+//  Copyright © 2016 MultiEducator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
